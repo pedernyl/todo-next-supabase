f1c2d07e24468c68b4c64a7c402a33a42a74321a
Peder Nylander <peder@pedernylander.se>
2025-09-03 15:43:57 +0200

    using mail auth session

--- src/lib/dataService.ts
+++ src/lib/dataService.ts
@@ -1,5 +1,8 @@
 import { supabase } from './supabaseClient';
 import { Todo } from '../../types';
+import { authOptions } from "../lib/authOptions";
+import { getServerSession } from "next-auth";
+
 
 // Fetch all todos from Supabase
 export async function getTodos(): Promise<Todo[]> {
@@ -14,9 +17,13 @@ export async function getTodos(): Promise<Todo[]> {
 
 // Create a new todo in Supabase
 export async function createTodo(title: string, description: string): Promise<Todo> {
+
+  const session = await getServerSession(authOptions);
+  if (!session) throw new Error("User not authenticated");
+  
   const { data, error } = await supabase
     .from('todos')
-    .insert([{ title, description, owned_by: 1,completed: false }])
+    .insert([{ title, description, owner_mail: session.user?.email, completed: false }])
     .select()
     .single();
 
